9b6fca268b4671093e6d8abad4fc09d2dc9d72e7
nootkatone <shenzhuofan01@163.com>
2017-05-17 15:55:42 +0800

    Add files via upload

---
+++ input.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "function.h"
+
+struct coordinate input(int round, char user1[], char user2[])
+{
+	struct coordinate position;
+	if(round % 2 == 0)
+		printf("Player %s, please input a position of chess piece: \n", user1);
+	else
+		printf("Player %s, please input a position of chess piece: \n", user2);
+	printf("Row = ");
+	scanf("%d", &position.r);
+	while(position.r < 1 || position.r >= M)
+	{
+		printf("Error! The position is invalid! \n");
+		printf("Row = ");
+		scanf("%d", &position.r);
+	}
+	printf("Column = ");
+	scanf("%d", &position.c);
+	while(position.c < 1 || position.c >= N)
+	{
+		printf("Error! The position is invalid! \n");
+		printf("Column = ");
+		scanf("%d", &position.c);
+	}
+	return position;
+}
---
+++ print.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "function.h"
+
+void print(int (* chess)[N])
+{
+	int row, column;
+	printf("\n");
+		for(row = 0; row < M; row ++)
+		{
+			for(column = 0; column < N; column ++)
+			{
+				if(row != 0 && column != 0)
+				{
+					switch(chess[row][column])
+					{
+					case 0:
+						printf(" ¡¤ ");
+						break;
+					case 1:
+						printf(" ¡ñ ");
+						break;
+					case 2:
+						printf(" ¡ð ");
+						break;
+					}
+				}
+				else
+				{
+					if(row == 0)
+					{
+						if(column < 10)
+							printf("  %d ", column);
+						else
+							printf(" %d ", column);
+					}
+					else
+					{
+						if(row < 10)
+							printf("  %d ", row);
+						else
+							printf(" %d ", row);
+					}
+				}
+			}
+			printf("\n\n");
+		}
+}
